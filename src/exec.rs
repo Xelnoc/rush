434c82e26c5ac945631766e896acbf8470ed7f95
xelnoc <haralds.meyers@protonmail.com>
2024-06-13 13:17:46 +0100

    separated modules into files

---
+++ src/exec.rs
@@ -0,0 +1,10 @@
+pub fn main(command:&str, args:&str, dir: String) {
+    if command == "cd" {
+
+    } else if command == "cat" {
+
+    } else {
+        println!("command '{command}' does not exist")
+    }
+}
++
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod exec;
+
 use std::env;
 use std::io;
 use std::io::Write;
+use substring::Substring;
+use std::str;
 
 fn readline() -> String {
     let mut input = String::new();
@@ -9,17 +13,16 @@ fn readline() -> String {
     input
 }
 
-fn exec(command:&str, args:&str) {
-
-}
 
 fn main() {
     let mut dir: String = env::var("HOME").unwrap();
-    while true {
-        let mut input = String::new();
-        print!("rush {} | ", dir);
-        io::stdout().flush();
+    loop {
+        print!("rush {} | ", &dir);
+        io::stdout().flush().expect("unable to flush");
         let input = readline();
-
+        let command: &str = input.substring(0, input.as_str().chars().position(|x: char| x.to_string() == " ").unwrap_or(input.len() - 1) + 1);
+        let args: &str = input.substring(input.as_str().chars().position(|x: char| x.to_string() == " ").unwrap() + 1, input.len() - 1);
+        println!("{args}");
+        exec::main(&command, &args, dir.clone())
     }
 }
