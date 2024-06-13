39dff1f71f01a4cb7a3659721569033fe893f8b6
xelnoc <haralds.meyers@protonmail.com>
2024-06-13 10:19:18 +0100

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::env;
+use std::io;
+use std::io::Write;
+
+fn readline() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+    input
+}
+
+fn exec(command:&str, args:&str) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("mothafucka, ich kusse deine schwester");
+    let mut dir: String = env::var("HOME").unwrap();
+    while true {
+        let mut input = String::new();
+        print!("rush {} | ", dir);
+        io::stdout().flush();
+        let input = readline();
+
+    }
 }
